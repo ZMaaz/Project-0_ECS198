2c514d7bfde3d35e53d47741db82564cf5469a57
mahrukh <mahrukhzubair@hotmail.com>
2022-03-31 15:47:01 -0700

    init Commit 2

---
+++ app/src/main/java/com/example/project0/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.project0
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle? ) {
+        title= "Hello World!"
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
